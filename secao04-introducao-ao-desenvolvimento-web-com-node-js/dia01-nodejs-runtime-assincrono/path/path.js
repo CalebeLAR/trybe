8e8186bf419a4e3e05adfa803ac7a326de81787c
CalebeLAR <ca6ele@gmail.com>
2023-02-19 12:49:19 -0400

    submodule my-node-project

--- secao04-introducao-ao-desenvolvimento-web-com-node-js/dia01-nodejs-runtime-assincrono/path/path.js
+++ secao04-introducao-ao-desenvolvimento-web-com-node-js/dia01-nodejs-runtime-assincrono/path/path.js
@@ -2,3 +2,5 @@ const path = require('path');
 console.log(path.dirname('/usr/shere/gnome'))
 console.log(path.resolve('usr','shere','gnome'))
 console.log(path.resolve('/', 'usr','shere','gnome'))
+console.log(path.resolve(__dirname))
+console.log(path.resolve(__filename))
