ed17611fc1f4cd7265f958e6cf850bce1ce00a66
calebeLAR <ca6ele@gmail.com>
2023-05-31 16:58:31 -0400

    p07

--- secao09-programacao-orientada-a-objetos-(poo)-e-solid/dia03/paginas/pagina07.ts
+++ secao09-programacao-orientada-a-objetos-(poo)-e-solid/dia03/paginas/pagina07.ts
@@ -4,25 +4,6 @@
   -------------------------------------- 
  **/
 const pagina07 = () => {
-  // Por vezes, criamos classes que devem possuir métodos pensados para serem implementados em subclasses.
-
-  // A ideia é que a superclasse, mais genérica, não deve fazer ideia de como esse método deve funcionar, apenas saber que ele existe.
-
-  // Isso é o mesmo que quando há uma implementação de interface, com a diferença na interface nenhum método é implementado, e aqui queremos selecionar alguns métodos para que não sejam implementados.
-
-  // Para isso, utilizamos classes abstratas, e estas possuem métodos abstratos.
-
-  // As classes abstratas não podem ser instanciadas, ou seja, você não pode criar um objeto a partir de uma classe abstrata.
-
-  // Métodos abstratos só podem existir em classes abstratas, e eles devem ser implementados na subclasse.
-
-  // No exemplo abaixo temos uma classe abstrata Employee que possui um atributo abstrato(MIN_SALARY) e um método abstrato (work()) que se aplica a seus subtipos. No caso da Trybe, temos pessoa instrutora, pessoa especialista e pessoa facilitadora. Portanto, ao criar as classes de cada subtipo, estendemos a partir da classe abstrata Employee e implementamos o atributo e o método de acordo com suas particularidades.
-
-  /*
-  Dicionário en-pt:
-  - employee: pessoa empregada/funcionária
-  */
-
   abstract class Employee {
     constructor(public name: string) { }
 
@@ -65,12 +46,7 @@ const pagina07 = () => {
 
   instructor.work(); // Victor está auxiliando as pessoas estudantes em mentorias.
   specialist.work(); // Gus está ministrando uma Aula Ao Vivo.
-  facilitator.work(); // Silvinha está conduzindo um 1:1.
-
-  // Veja na prática como funcionam as classes abstratas:
-  // Para fixar
-
-  // Suponha que você está modelando os personagens do jogo de luta multijogadores Super Smash Bros. No jogo, há personagens de alcances variados e movimentos especiais variados. Vence a pessoa que acumulou mais pontos na partida.
+  facilitator.work(); // Silvinha está conduzindo um 1:
 
   const parafixar01 = () => {
     // 1 - Crie uma classe abstrata Character que tenha os métodos abstratos talk(): void e specialMove(): void.
