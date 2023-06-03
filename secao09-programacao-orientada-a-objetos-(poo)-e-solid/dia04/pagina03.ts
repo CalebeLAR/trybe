f15fb7f66fddb3f13db5400a8cc728e95ce9d704
calebeLAR <ca6ele@gmail.com>
2023-06-03 19:26:22 -0400

    pagina03

--- secao09-programacao-orientada-a-objetos-(poo)-e-solid/dia04/pagina03.ts
+++ secao09-programacao-orientada-a-objetos-(poo)-e-solid/dia04/pagina03.ts
@@ -2,5 +2,6 @@ const pagina03 = () => {
   // -------------------------------
   // Single Responsibility Principle
   // -------------------------------
+  // SRP -> Single responsibility principle (Princípio da responsabilidade única): uma entidade (classe, método, função, etc) deve ter apenas uma única responsabilidade; 
   // conteúdo para fixar no diretorio pagina03;
 }
