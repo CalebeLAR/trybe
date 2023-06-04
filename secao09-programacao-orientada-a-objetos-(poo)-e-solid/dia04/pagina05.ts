675f08a95ac72c39c63615828fb1b948a8178959
calebeLAR <ca6ele@gmail.com>
2023-06-04 14:46:03 -0400

    pagina05

---
+++ secao09-programacao-orientada-a-objetos-(poo)-e-solid/dia04/pagina05.ts
@@ -0,0 +1,7 @@
+const pagina05 = () => {
+  // ------------------------------
+  // Dependency Inversion Principle
+  // ------------------------------
+  // LSP -> LISKOV SUBSTITUITION PRINCIPLE (princípio da substituição de liskov)
+  // Entidades de alto nível não devem depender de entidades de baixo nível. Ambos devem depender de abstrações.   
+};
