0c746c9a132cf14c6b988dc318af44e67650ce02
calebeLAR <ca6ele@gmail.com>
2023-06-12 15:48:26 -0400

    pagina04

---
+++ secao10-projeto-tfc-trybe-futebol-clube/dia01/pagina04.c.ts
@@ -0,0 +1,24 @@
+class pagina04 {
+  constructor(
+    // ----------------------------------
+    private Configurando_Sequelize = true
+    // ----------------------------------
+  ){}
+
+  static Criando_a_Primeira_Interface() {
+    // src/interfaces/books/IBook.ts
+  }
+
+  static Configurando_as_Migrations() {
+    // database/migrations/01-create-books-table.ts
+  }
+
+  static Configurando_as_Seeders() {
+    // src/database/seeders/book.ts
+  }
+
+  static Configurando_a_Model_do_Sequelize() {
+    // src/database/models/SequelizeBook
+  }
+
+}
