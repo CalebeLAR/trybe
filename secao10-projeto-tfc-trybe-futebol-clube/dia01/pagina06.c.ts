ccf9e857c08ee14162b12851e7426b4a2b98c121
calebeLAR <ca6ele@gmail.com>
2023-06-18 13:45:50 -0400

    finaliza dia

---
+++ secao10-projeto-tfc-trybe-futebol-clube/dia01/pagina06.c.ts
@@ -0,0 +1,19 @@
+class Pagina06 {
+  static Atualizando_um_livro(){}
+  static Criando_a_Model(){
+    // src/interfaces/books/IBookModel.ts
+    // src/models/BookModel.ts
+  }
+  static Criando_a_Service(){
+    // src/services/BookService.ts`
+  }
+  static Criando_a_Controller(){
+    // src/controllers/BookController.ts
+  }
+  static Criando_a_rota_de_atualização_de_livros(){
+    // src/routes/books.routes.ts
+  }
+  static Criando_testes_de_integração(){
+    // tests/integration/Book.test.ts
+  }
+}
---
+++ secao10-projeto-tfc-trybe-futebol-clube/dia01/pagina08.c.ts
@@ -0,0 +1,10 @@
+class pagina08 {
+  declare Conclusão: string;
+
+  static resumo() {
+    // Como implementar uma API em Node.js utilizando os conceitos de POO;
+    // Criamos interfaces para definir os contratos de cada entidade com que trabalhamos;
+    // Criamos classes para as camadas de Model, Service e Controller, deixando o mais genérico e reutilizável possível e;
+    // Implementamos testes de integração para todas as rotas de forma a garantir o funcionamento correto do nosso código.
+  };
+}
