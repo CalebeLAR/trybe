c2c8c390dcb5cd189374f9e09baafa518155e997
calebeLAR <ca6ele@gmail.com>
2023-07-11 20:33:03 -0400

    trybe.CS.S3.D2

---
+++ secao03-estrutura-de-dados-i-listas-lineares/dia02-arrays/pagina04.fx.py
@@ -0,0 +1,11 @@
+class ParaFixarI:
+    def exercício01():
+        """ Tipos abstratos de dados possuem uma única implementação
+        bem documentada? """
+        print('Não necessariamente.')
+
+    def exercício02():
+        """ Listas(list), dicionários(dict) e conjuntos(set), que já vêm no
+        Python, são considerados tipos de dados? Caso negativo, justifique sua
+        resposta? """
+        print('Sim, as listas list, dicionários dict e conjuntos set são considerados tipos de dados em Python.')
