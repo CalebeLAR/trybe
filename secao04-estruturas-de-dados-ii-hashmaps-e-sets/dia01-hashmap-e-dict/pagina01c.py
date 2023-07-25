711ecf95640065a180d585322fbb9f1fd3bafc55
calebeLAR <ca6ele@gmail.com>
2023-07-25 14:04:30 -0400

    trybe.CS.S3.D3(78%)

---
+++ secao04-estruturas-de-dados-ii-hashmaps-e-sets/dia01-hashmap-e-dict/pagina01c.py
@@ -0,0 +1,12 @@
+class Pagina01c:
+    def introducao():
+        pass
+
+    def o_que_vamos_aprender():
+        pass
+
+    def você_será_capaz_de():
+        pass
+
+    def por_que_isso_é_importante():
+        pass
