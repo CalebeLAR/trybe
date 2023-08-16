42ac27d062c098f17ecedb11b8e945a069f76087
calebeLAR <ca6ele@gmail.com>
2023-08-16 10:16:43 -0400

    renameDir: trybe.PY.S1

---
+++ secao01-debug-com-python/dia01-estrategias-de-debug-com-python/pagina04c.py
@@ -0,0 +1,25 @@
+from pagina04.para_fixar01 import ParaFixar01
+
+
+class Pagina04:
+    def Visualizando_dados_da_depuração():
+        pass
+
+    def Janela_VARIABLES_(variáveis):
+        pass
+
+    def Janela_CALL_STACK_(pilha_de_chamadas):
+        pass
+
+    def Janela_WATCH_(inspeção):
+        pass
+
+    def Para_fixarI():
+        print('=========== paraFixar I ===========')
+        ParaFixar01.exercicio01()
+        print('=========== paraFixar II ===========')
+        ParaFixar01.exercicio02()
+
+
+if __name__ == '__main__':
+    Pagina04.Para_fixarI()
