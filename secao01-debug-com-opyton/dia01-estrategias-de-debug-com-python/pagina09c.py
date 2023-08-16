228c38634e8713de72f4645aa663d9e2cf29f67a
calebeLAR <ca6ele@gmail.com>
2023-08-16 10:10:37 -0400

    trybe.CS.S1.D1

--- secao01-debug-com-opyton/dia01-estrategias-de-debug-com-python/pagina09c.py
+++ secao01-debug-com-opyton/dia01-estrategias-de-debug-com-python/pagina09c.py
@@ -3,14 +3,13 @@ class Pagina09:
         pass
 
     def Referências():
-        print('Documentação do VS Code sobre o debugger do VS Code')
+        print("Documentação do VS Code sobre o debugger do VS Code")
         """https://code.visualstudio.com/docs/editor/debugging
         Debugging
         """
-        print('Documentação do VS Code sobre o debugger do Python')
+        print("Documentação do VS Code sobre o debugger do Python")
         """https://code.visualstudio.com/docs/python/debugging
         Python debugging in VS Code
         """
-        print('Debug ou Depuração: o que é e para que serve? (Blog da Trybe)')
+        print("Debug ou Depuração: o que é e para que serve? (Blog da Trybe)")
         """https://blog.betrybe.com/tecnologia/debug/"""
-
