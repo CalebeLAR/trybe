cb998467118fd9d129dbdd8d78b33dbe4d6b3cf8
calebeLAR <ca6ele@gmail.com>
2023-09-11 11:34:23 -0400

    trybe.python.seção03.dia03

---
+++ secao03-raspagem-de-dados/dia03-mongodb-e-crawlers-aula-ao-vivo/pagina11/pagina11c.py
@@ -0,0 +1,11 @@
+class Pagina11:
+    def MongoDB_e_Crawlers():
+        # python3 -m venv .venv && source .venv/bin/activate
+        # python3 -m pip install pymongo
+
+        # p11_pymongo.py
+
+        # https://docs.mongodb.com/
+        pass
+
+    pass
