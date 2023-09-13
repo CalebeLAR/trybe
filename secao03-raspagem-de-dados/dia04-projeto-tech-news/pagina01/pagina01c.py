cb7ea5f2d47f627711b5c26f00a8f27a832e409b
calebeLAR <ca6ele@gmail.com>
2023-09-13 18:57:44 -0400

    trybe.python.seção03.dia04

---
+++ secao03-raspagem-de-dados/dia04-projeto-tech-news/pagina01/pagina01c.py
@@ -0,0 +1,21 @@
+class Pagina01:
+    def O_que_vamos_fazer():
+        # requests, parsel, BeautifulSoup e Selenium
+        pass
+
+    def Por_que_isso_é_importante():
+        pass
+
+    def O_que_vamos_avaliar():
+        pass
+
+    def O_que_devo_desenvolver():
+        pass
+
+    def Entregáveis():
+        pass
+
+    def Combinados():
+        pass
+
+    pass
