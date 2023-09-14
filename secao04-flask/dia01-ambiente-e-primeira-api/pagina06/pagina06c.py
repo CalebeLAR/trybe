f591521d614e89ffa31dc055f9c45ac68fe533e7
calebeLAR <ca6ele@gmail.com>
2023-09-14 12:48:31 -0400

    trybe.python.seção04.dia01.pagina06

---
+++ secao04-flask/dia01-ambiente-e-primeira-api/pagina06/pagina06c.py
@@ -0,0 +1,20 @@
+class Pagina06:
+    def Primeira_API_em_Flask():
+        pass
+
+    def Arquitetura_MVC():
+        # mkdir -p src/controllers && touch src/controllers/jokes_controller.py && # noqa
+        # mkdir -p src/models && touch src/models/db.py &&
+        # mkdir -p src/models && touch src/models/abstract_model.py &&
+        # mkdir -p src/models && touch src/models/joke_model.py
+        pass
+
+    def Models():
+        # * p06_a01_src_models_db_v00 copy.py
+        # * p06_a02_src_models_jokemodel_v00.py
+        # * p06_a03_src_controllers_abstract_model.v00.py
+
+        # db["jokes"].find({"_id": 1000}) == "select * from jokes where id = 1000;" # noqa
+        pass
+
+    pass
