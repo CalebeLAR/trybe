7f37992c3b4d1e091007936f39379a9db92cad13
calebeLAR <ca6ele@gmail.com>
2023-09-19 15:19:38 -0400

    trybe.python.seção04.dia01.pagina10

---
+++ secao04-flask/dia01-ambiente-e-primeira-api/pagina10/pagina10c.py
@@ -0,0 +1,16 @@
+class Pagina09:
+    def Referências_e_recursos_adicionais_(opcional):
+        pass
+
+    def Referências():
+        # GRINBERG, M. Flask Web Development: Developing Web Applications with Python. Editora O’Reilly Media. 2018.  # noqa: E501
+        # AGGARWAL, S. Flask Framework Cookbook, Second Edition: Over 80 proven recipes and techniques for Python web development with Flask, 2nd Edition. Editora Packt Publishing. 2019.  # noqa: E501
+
+        # Documentação do Flask.
+        # https://flask.palletsprojects.com/en/2.3.x/
+        # Documentação do Werkzeug.
+
+        # https://werkzeug.palletsprojects.com/en/2.3.x/
+        pass
+
+    pass
