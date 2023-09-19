7745ea47c8cf7dd81052fdbd424aba0e03f139e7
calebeLAR <ca6ele@gmail.com>
2023-09-19 17:15:11 -0400

    trybe.python.seção04.dia02.pagina03

---
+++ secao04-flask/dia02-server-side-rendering-em-flask/pagina03.py/pagina03c.py
@@ -0,0 +1,32 @@
+class Pagina03:
+    def O_que_é_o_Jinja2():
+        pass
+
+    def Hello_World_profissional_com_Jinja2():
+        # git clone git@github.com:tryber/python-conteudo-flask-trybot.git
+        pass
+
+    def Programando_o_app_ponto_py():
+        # /p03_app_v01.py
+        pass
+
+    def Programando_a_home_controller_ponto_py():
+        # /home_controller_v01.py
+        pass
+
+    def Definindo_o_primeiro_template():
+        # /index_html_v01.py
+        pass
+
+    def Subindo_a_aplicação():
+        # docker compose up flask-api
+        pass
+
+    def Customizando_a_mensagem_com_o_Jinja2():
+        # /home_controller_v02.py
+
+        # Filtros no Jinja2
+        # https://jinja.palletsprojects.com/en/3.1.x/templates/#filters
+        pass
+
+    pass
