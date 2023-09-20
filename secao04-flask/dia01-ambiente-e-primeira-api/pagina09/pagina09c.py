0f03304d19c1a2fc63a0c046f776405a90618b30
calebeLAR <ca6ele@gmail.com>
2023-09-20 13:27:52 -0400

    trybe.python.seção04.dia01.pagina09

--- secao04-flask/dia01-ambiente-e-primeira-api/pagina09/pagina09c.py
+++ secao04-flask/dia01-ambiente-e-primeira-api/pagina09/pagina09c.py
@@ -20,6 +20,19 @@ class Pagina09:
         pass
 
     def Exercício03():
+        # Em nossas jokes_controller e musics_controller, retornamos os números
+        #  fixos dos status HTTP:
+        #   200 - OK
+        #   201 - Created
+        #   204 - No Content
+        #   404 - Not Found
+        # Faça uma refatoração na hora de retornar o status HTTP, de forma que
+        #  não sejam utilizados números mágicos nos status.
+
+        # Sugestão, refatore usando o
+        #  <enum>(https://docs.python.org/3/library/enum.html)
+        #  do Python.
+
         pass
 
     pass
