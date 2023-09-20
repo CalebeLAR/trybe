6543406d6f51ba1c539d3ed0fefafc6dce7489e3
calebeLAR <ca6ele@gmail.com>
2023-09-20 18:30:37 -0400

    trybe.python.seção04.dia02.pagina06

---
+++ secao04-flask/dia02-server-side-rendering-em-flask/pagina06.py/pagina06c.py
@@ -0,0 +1,29 @@
+class Pagina05:
+    def Para_fixar():
+        pass
+
+    def Exercício01():
+        # Crie um aplicativo Flask que renderize um template chamado
+        # index.html. O template deve exibir uma mensagem de boas-vindas na
+        # página. A mensagem de boas-vindas deve ser passada como um argumento
+        # para a função render_template. Além disso, certifique-se de que o
+        # template esteja corretamente configurado com a estrutura básica do
+        # HTML.
+        pass
+
+    def Exercício02():
+        pass
+
+    def Exercício03():
+        pass
+
+    def Exercício04():
+        pass
+
+    def Exercício05():
+        pass
+
+    def Exercício06():
+        pass
+
+    pass
