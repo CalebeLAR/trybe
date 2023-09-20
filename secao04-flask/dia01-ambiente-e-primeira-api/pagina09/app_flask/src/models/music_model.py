31454e1e8e96b4f406dd691025c1c8fe3733a8b6
calebeLAR <ca6ele@gmail.com>
2023-09-20 12:09:48 -0400

    trybe.python.seção04.dia01.pagina09.exercicio01

---
+++ secao04-flask/dia01-ambiente-e-primeira-api/pagina09/app_flask/src/models/music_model.py
@@ -0,0 +1,12 @@
+# music_model
+from db import db
+from abstract_model import AbstractModel
+
+
+class MusicModel(AbstractModel):
+    _collection = db["music"]
+
+    def __init__(self, json_data: dict):
+        super().__init__(json_data)
+
+    pass
---
+++ secao04-flask/dia01-ambiente-e-primeira-api/pagina09/pagina09c.py
@@ -0,0 +1,24 @@
+class Pagina09:
+    def Vamos_praticar():
+        pass
+
+    def Exercício01():
+        # Crie uma nova Blueprint Controller semelhante à API de piadas,
+        # para criar uma coleção de sugestões de músicas na base de dados.
+
+        # Passos:
+        #     Crie a music_model.py (Implementação equivalente a joke_model.py)
+        #     Crie a musics_controller.py e registre como Blueprint.
+        #         Registre em musics_controller.py e app.py, semelhante ao
+        #           realizado para a API no exemplo do conteúdo.
+        #     Crie uma rota POST para salvar uma música.
+
+        pass
+
+    def Exercício02():
+        pass
+
+    def Exercício03():
+        pass
+
+    pass
