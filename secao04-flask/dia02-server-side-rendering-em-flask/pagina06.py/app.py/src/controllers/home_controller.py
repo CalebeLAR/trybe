4d5a177c78f99304126faf4147d18f310aefbd2a
calebeLAR <ca6ele@gmail.com>
2023-09-21 18:37:58 -0400

    trybe.python.seção04.dia02.pagina06: exercicio01

--- secao04-flask/dia02-server-side-rendering-em-flask/pagina06.py/app.py/src/controllers/home_controller.py
+++ secao04-flask/dia02-server-side-rendering-em-flask/pagina06.py/app.py/src/controllers/home_controller.py
@@ -7,4 +7,5 @@ home_controller = Blueprint("home", __name__)
 # routers
 @home_controller.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    message_prop = "BOAS VINDAS"
+    return render_template("index.html", message_prop=message_prop)
