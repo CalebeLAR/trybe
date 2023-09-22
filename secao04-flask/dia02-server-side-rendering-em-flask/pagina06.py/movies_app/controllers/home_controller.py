d62dc0fcfd6e913cb2f93fb86adb04e4ea68fd70
calebeLAR <ca6ele@gmail.com>
2023-09-22 10:02:44 -0400

    trybe.python.seção04

--- secao04-flask/dia02-server-side-rendering-em-flask/pagina06.py/movies_app/controllers/home_controller.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-home_controller = Blueprint("home_controller", __name__)
-
-
-@home_controller.route("/", methods=["GET"])
-def home():
-    return render_template("index.html")
