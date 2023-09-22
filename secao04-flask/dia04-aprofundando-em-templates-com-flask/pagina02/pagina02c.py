2a746eb56e43a56cd5b28d7698f016caa2ece126
calebeLAR <ca6ele@gmail.com>
2023-09-22 11:37:10 -0400

    trybe.python.seção04.dia04.pagina02

---
+++ secao04-flask/dia04-aprofundando-em-templates-com-flask/pagina02/pagina02c.py
@@ -0,0 +1,12 @@
+class Pagina02:
+    def API_do_Jinja2():
+        pass
+
+    def Funcionamento_do_Jinja():
+        # pip install Jinja2
+
+        # documentação oficial jinja2
+        # https://jinja.palletsprojects.com/
+        pass
+
+    pass
