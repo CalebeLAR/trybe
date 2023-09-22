e66feddf858c5dcaa1dc233d872357ca4bc464c8
calebeLAR <ca6ele@gmail.com>
2023-09-22 09:34:02 -0400

    trybe.python.seção04.dia02.pagina04

---
+++ secao04-flask/dia02-server-side-rendering-em-flask/pagina04/pagina04c.py
@@ -0,0 +1,13 @@
+class Pagina04:
+    def Exemplo_de_Chat_Model_e_View():
+        pass
+
+    def Criando_a_model_do_Chat():
+        # p03_message_model.py_v01.py
+        pass
+
+    def Criando_a_view_do_Chat():
+        # p03_chat.html_v01.py
+        pass
+
+    pass
