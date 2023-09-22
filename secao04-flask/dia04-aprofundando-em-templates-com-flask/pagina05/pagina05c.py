ea91733b07a4dc750643536262b2e4e2fda5b8ea
calebeLAR <ca6ele@gmail.com>
2023-09-22 11:38:37 -0400

    trybe.python.seção04.dia04.pagina05

---
+++ secao04-flask/dia04-aprofundando-em-templates-com-flask/pagina05/pagina05c.py
@@ -0,0 +1,15 @@
+class Pagina05:
+    def Filtros_customizados_no_Jinja2():
+        pass
+
+    def Criando_filtros_customizados():
+        # documentação sobre a strptime.
+        # https://docs.python.org/3/library/datetime.html#datetime.datetime.strptime
+        # documentação sobre a strftime.
+        # https://docs.python.org/3/library/datetime.html#datetime.date.strftime
+        pass
+
+    def Filtros_com_mais_de_um_parâmetro():
+        pass
+
+    pass
