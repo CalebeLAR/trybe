81e3f6489644f3cf8a7511a1270d121e87f2e2ae
calebeLAR <ca6ele@gmail.com>
2023-09-23 10:24:38 -0400

    trybe.python.seção04.dia02.pagina06.exercicio06

--- secao04-flask/dia02-server-side-rendering-em-flask/pagina06/pagina06c.py
+++ secao04-flask/dia02-server-side-rendering-em-flask/pagina06/pagina06c.py
@@ -12,18 +12,30 @@ class Pagina05:
         pass
 
     def Exercício02():
+        # Crie um aplicativo Flask que renderize um template chamado
+        # ‘book.html’. O template deve receber informações de um livro, como
+        # título, autor e ano de publicação, e exibi-las na página.
         pass
 
     def Exercício03():
+        # Crie um aplicativo web, usando Flask e Jinja2, que exiba uma lista
+        # de filmes. Acompanhe o passo a passo:
         pass
 
     def Exercício04():
+        # Crie um aplicativo Flask para gerenciar uma lista de produtos. O
+        # aplicativo deve ter duas rotas principais: uma para exibir a lista
+        # de produtos e outra para adicionar novos produtos.
         pass
 
     def Exercício05():
+        # Dando sequência ao exercício anterior, construa a view desta
+        # aplicação:
         pass
 
     def Exercício06():
+        # Para finalizar e dar sequência ao exercício anterior, construa o
+        # controller desta aplicação:
         pass
 
     pass
