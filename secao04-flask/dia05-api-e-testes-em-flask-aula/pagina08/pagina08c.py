ae47c003b118dfffc22f7e79f9f7273c1ef83f23
calebeLAR <ca6ele@gmail.com>
2023-09-23 16:12:57 -0400

    trybe.python.seção04.dia05.pagina08

---
+++ secao04-flask/dia05-api-e-testes-em-flask-aula/pagina08/pagina08c.py
@@ -0,0 +1,17 @@
+class Pagina08:
+    def Referências_e_recursos_adicionais(opcional):
+        pass
+
+    def Referências():
+        # Documentação do Flask
+        # https://docs.python.org/3/library/unittest.html
+
+        # Documentação do unittest
+        # https://docs.pytest.org/en/stable/
+
+        # Documentação do pytest
+        # https://flask.palletsprojects.com/en/1.1.x/
+
+        pass
+
+    pass
