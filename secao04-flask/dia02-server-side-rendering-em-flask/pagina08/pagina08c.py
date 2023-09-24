273899cbfe97e0b1387f6cea2fb1112d759789c2
calebeLAR <ca6ele@gmail.com>
2023-09-24 17:36:31 -0400

    trybe.python.seção04.dia02.pagina08.exercício03

--- secao04-flask/dia02-server-side-rendering-em-flask/pagina08/pagina08c.py
+++ secao04-flask/dia02-server-side-rendering-em-flask/pagina08/pagina08c.py
@@ -1,15 +1,17 @@
 class Pagina07:
     def Exercício01():
-        # 1 - Crie uma classe chamada Task que representa uma tarefa, com os
+        # Crie uma classe chamada Task que representa uma tarefa, com os
         # atributos id, title e completed.
         pass
 
     def Exercício02():
-        # 2 - Crie uma lista de tarefas no arquivo do aplicativo e
+        # Crie uma lista de tarefas no arquivo do aplicativo e
         # inicialize-a com algumas tarefas de exemplo.
         pass
 
     def Exercício03():
+        # Crie um template chamado tasks.html que exiba a lista de tarefas e
+        # forneça um formulário para adicionar novas tarefas.
         pass
 
     def Exercício04():
