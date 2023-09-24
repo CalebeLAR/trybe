cee1537f56b0e3fb8c2033f924d03b77c0ce9eb9
calebeLAR <ca6ele@gmail.com>
2023-09-24 17:06:20 -0400

    trybe.python.seção04.dia02.pagina08.exercício02

--- secao04-flask/dia02-server-side-rendering-em-flask/pagina08/todo_list_app/app.py
+++ secao04-flask/dia02-server-side-rendering-em-flask/pagina08/todo_list_app/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from task_model import Task
+
+
+tasks = [
+    Task(1, "estudar", True),
+    Task(2, "fazer exercícios", False),
+    Task(3, "ler um capítulo", True),
+]
 
 app = Flask(__name__)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
