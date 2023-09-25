80205d72c58a2834828493ed5d66fb6a5aea790e
calebeLAR <ca6ele@gmail.com>
2023-09-25 11:50:57 -0400

    trybe.python.seção05.dia01.pagina04

---
+++ secao05_django_1/dia01-ambiente-de-banco-de-dados-e-orm/pagina04/pagina04c.py
@@ -0,0 +1,24 @@
+class Pagina04:
+    def Primeiro_projeto_Django():
+        pass
+
+    def Preparando_o_ambiente():
+        # pip install django
+        # django-admin startproject ecommerce .
+        pass
+
+    def Executando_o_projeto():
+        # python3 manage.py runserver
+        # SQLite viewer
+        pass
+
+    def Entendendo_a_estrutura_do_projeto():
+        pass
+
+    def Arquivo_settings_ponto_py():
+        pass
+
+    def Arquivo_urls_ponto_py():
+        pass
+
+    pass
