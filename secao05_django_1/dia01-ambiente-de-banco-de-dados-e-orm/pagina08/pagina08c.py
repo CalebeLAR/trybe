7e9c8127a22a2f044872c542ad99ec0764a1a600
calebeLAR <ca6ele@gmail.com>
2023-10-04 18:00:12 -0300

    trybe.python.seção05.dia01.pagina08

---
+++ secao05_django_1/dia01-ambiente-de-banco-de-dados-e-orm/pagina08/pagina08c.py
@@ -0,0 +1,16 @@
+class Pagina08:
+    def Django_admin():
+        pass
+
+    def Criando_um_superusuário():
+        # python3 manage.py createsuperuser
+        pass
+
+    def Registrando_o_modelo():
+        pass
+
+    def Para_fixar():
+        # 3. Utilizando o painel de administração do Django, altere o produto cadastrado Moka  # noqa: E501
+        pass
+
+    pass
