bc38455f53acfc9bb2f10da36cc9c35cbb934241
calebeLAR <ca6ele@gmail.com>
2023-10-05 18:28:18 -0300

    trybe.python.seção05.dia02.pagina06

---
+++ secao05_django_1/dia02-templates-do-django/pagina06/pagina06c.py
@@ -0,0 +1,19 @@
+class Pagina06:
+    def Criando_o_template_de_detalhes_do_evento():
+        # https://docs.djangoproject.com/en/4.2/ref/templates/builtins/#built-in-filter-reference
+        pass
+
+    def Para_fixar():
+        pass
+
+    def Exercício_4():
+        # Adicione um novo evento e acesse a sua rota de detalhes para comprovar que tudo está funcionando.
+        pass
+
+    def Conectando_a_página_inicial_com_a_página_de_detalhes():
+        pass
+
+    def Lidando_com_exceções():
+        pass
+
+    pass
