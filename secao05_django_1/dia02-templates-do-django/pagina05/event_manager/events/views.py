759228f0ffcb7ea1b2ed0487a336ad5e921991c2
calebeLAR <ca6ele@gmail.com>
2023-10-05 18:28:13 -0300

    trybe.python.seção05.dia02.pagina05

---
+++ secao05_django_1/dia02-templates-do-django/pagina05/event_manager/events/views.py
@@ -0,0 +1,12 @@
+# events/views.py
+from events.models import Event
+from django.shortcuts import render
+
+
+def index(request):
+    context = {"company": "Trybe", "events": Event.objects.all()}
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    return render(request, "about.html")
---
+++ secao05_django_1/dia02-templates-do-django/pagina05/pagina05c.py
@@ -0,0 +1,19 @@
+class Pagina05:
+    def Trabalhando_com_elementos_do_banco_usando_Python():
+        pass
+
+    def Renderizando_os_eventos_no_template():
+        pass
+
+    def Para_fixar():
+        pass
+
+    def Exercício02():
+        # Adicione mais 1 entrada no banco de dados, dentro da tabela events utilizando cada um dos métodos mostrados.  # noqa: E501
+        pass
+
+    def Exercício03():
+        # Remova todas as entradas que estão no banco para conseguir visualizar a mensagem dizendo que não existem eventos cadastrados no banco.  # noqa: E501
+        pass
+
+    pass
