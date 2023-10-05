31542b168c83215d0abd5f9f591023a92ba362e3
calebeLAR <ca6ele@gmail.com>
2023-10-05 12:37:32 -0300

    trybe.python.seção05.dia01.pagina02

---
+++ secao05_django_1/dia02-templates-do-django/pagina02/pagina02c.py
@@ -0,0 +1,14 @@
+class Pagina02:
+    def Templates_no_Django():
+        pass
+
+    def Jinja2_X_Django_Template_Language_DTL():
+        pass
+
+    def Configuração_de_Templates_no_Django():
+        pass
+
+    def Próximos_passos():
+        pass
+
+    pass
