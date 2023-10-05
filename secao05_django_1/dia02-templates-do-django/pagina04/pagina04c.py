d776e5e793a37f7d6e4a3e7d9d90b01a0678d446
calebeLAR <ca6ele@gmail.com>
2023-10-05 18:28:04 -0300

    trybe.python.seção05.dia02.pagina04

---
+++ secao05_django_1/dia02-templates-do-django/pagina04/pagina04c.py
@@ -0,0 +1,11 @@
+class Pagina04:
+    def Aprofundando_no_uso_de_templates():
+        pass
+
+    def Herança_de_templates():
+        pass
+
+    def Criando_o_model_Event():
+        pass
+
+    pass
