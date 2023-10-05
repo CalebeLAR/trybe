02b76cfadede4fa52f0f1b7caa1924a99c6cb66d
calebeLAR <ca6ele@gmail.com>
2023-10-05 12:37:44 -0300

    trybe.python.seção05.dia01.pagina03

---
+++ secao05_django_1/dia02-templates-do-django/pagina03/pagina03c.py
@@ -0,0 +1,17 @@
+class Pagina03:
+    def Colocando_o_primeiro_template_para_funcionar():
+        pass
+
+    def Setup_inicial():
+        pass
+
+    def Renderizando_seu_primeiro_template():
+        pass
+
+    def Para_fixar():
+        pass
+
+    def Exercício01():
+        pass
+
+    pass
