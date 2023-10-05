521f2abf908855ae79339020a088ba9609cede30
calebeLAR <ca6ele@gmail.com>
2023-10-05 00:05:20 -0300

    trybe.python.seção05.dia01.pagina11

---
+++ secao05_django_1/dia01-ambiente-de-banco-de-dados-e-orm/pagina11/pagina11c.py
@@ -0,0 +1,25 @@
+class Pagina11:
+    def Vamos_praticar():
+        pass
+
+    def Exercício01():
+        # Crie e execute um novo projeto Django
+        pass
+
+    def Exercício02():
+        # Altere o banco de dados para MySQL
+        pass
+
+    def Exercício03():
+        # Crie uma nova aplicação
+        pass
+
+    def Exercício04():
+        # Crie uma nova tabela no banco de dados
+        pass
+
+    def Exercício05():
+        # Crie um novo superusuário e faça login no Django Admin
+        pass
+
+    pass
