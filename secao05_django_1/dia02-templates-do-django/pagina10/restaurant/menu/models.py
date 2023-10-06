a6e462a39247ed2e9b432dcf76d0b1f2cc279642
calebeLAR <ca6ele@gmail.com>
2023-10-06 10:54:45 -0300

    trybe.python.seção05.dia02.pagina10.exercício01

---
+++ secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    image = models.CharField(max_length=400)
+    pass
