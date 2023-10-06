3b9de78edd3d92991e3ed4e386a212b1bd3be7d6
calebeLAR <ca6ele@gmail.com>
2023-10-06 15:26:27 -0300

    trybe.python.seção05.dia02.pagina07

---
+++ secao05_django_1/dia02-templates-do-django/pagina07/pagina07c.py
@@ -0,0 +1,13 @@
+class Pagina07:
+    def Aprimorando_os_templates():
+        pass
+
+    def Arquivos_estáticos():
+        # pip install whitenoise # Serve os arquivos estáticos a partir de um diretório
+        # pip install django-static-autocollect # Coleta os arquivos estáticos e os coloca em um diretório/
+
+
+        # https://whitenoise.readthedocs.io/en/latest/django.html#use-a-content-delivery-network
+        pass
+
+    pass
