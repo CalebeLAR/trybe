4d5fe251dec9edb53a52f5e72f65598758d9b607
calebeLAR <ca6ele@gmail.com>
2023-10-08 15:16:15 -0300

    trybe.python.seção05.dia02.pagina10

--- secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/urls.py
+++ secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from menu.views import index, recipe_details
+from menu.views import index, recipe_details, delete
 
 urlpatterns = [
     path("", index, name="index"),
     path("recipe/<int:id>", recipe_details, name="details-page"),
+    path("recipe/<int:id>/delete", delete, name="delete-page"),
 ]
--- secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/views.py
+++ secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/views.py
@@ -11,9 +11,16 @@ def recipe_details(request, id):
     recipe = Recipe.objects.get(id=id)
     context = {
         "recipe_name": recipe.name,
+        "recipe_id": id,
         "ingredients": recipe.ingredients.split(" "),
         "instructions": recipe.instructions.split(" "),
         "image_url": recipe.image,
     }
 
     return render(request, "details.html", context=context)
+
+
+def delete(request, id):
+    Recipe.objects.get(id=id).delete()
+    context = {"recipes": Recipe.objects.all()}
+    return render(request, "home.html", context=context)
