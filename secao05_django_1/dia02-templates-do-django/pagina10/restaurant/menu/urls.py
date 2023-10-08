02c5460799acc3ecbde8e6aee9ed6faa87ab9e74
calebeLAR <ca6ele@gmail.com>
2023-10-08 14:54:05 -0300

    trybe.python.seção05.dia02.pagina10.exercicio03

--- secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/urls.py
+++ secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from menu.views import index
+from menu.views import index, recipe_details
 
 urlpatterns = [
     path("", index, name="index"),
+    path("recipe/<int:id>", recipe_details, name="details-page"),
 ]
--- secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/views.py
+++ secao05_django_1/dia02-templates-do-django/pagina10/restaurant/menu/views.py
@@ -4,4 +4,16 @@ from menu.models import Recipe
 
 def index(request):
     context = {"recipes": Recipe.objects.all()}
-    return render(request, "home.components.html", context=context)
+    return render(request, "home.html", context=context)
+
+
+def recipe_details(request, id):
+    recipe = Recipe.objects.get(id=id)
+    context = {
+        "recipe_name": recipe.name,
+        "ingredients": recipe.ingredients.split(" "),
+        "instructions": recipe.instructions.split(" "),
+        "image_url": recipe.image,
+    }
+
+    return render(request, "details.html", context=context)
