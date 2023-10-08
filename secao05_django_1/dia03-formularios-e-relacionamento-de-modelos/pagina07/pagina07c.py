0ecaf381df30d01fa80652777e7d218d0c313981
calebeLAR <ca6ele@gmail.com>
2023-10-08 15:17:34 -0300

    trybe.python.seção05.dia03.pagina07

---
+++ secao05_django_1/dia03-formularios-e-relacionamento-de-modelos/pagina07/pagina07c.py
@@ -0,0 +1,16 @@
+class Pagina07:
+    def Enviando_dados_do_template_para_a_view():
+        pass
+
+    def Criando_o_novo_registro():
+        pass
+
+    def Para_fixar():
+        pass
+
+    def Exercício04():
+        # Refaça o procedimento explicado para a criação de objetos do tipo
+        # Music para o tipo Singer com a função singer
+        pass
+
+    pass
