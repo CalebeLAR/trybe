bf8c4769828604e46e2df34666644f44e8d4f7f1
calebeLAR <ca6ele@gmail.com>
2023-10-08 15:16:55 -0300

    trybe.python.seção05.dia03.pagina02

---
+++ secao05_django_1/dia03-formularios-e-relacionamento-de-modelos/pagina02/pagina02c.py
@@ -0,0 +1,18 @@
+class Pagina02:
+    def Setup_inicial():
+        # python3 -m venv .venv && source .venv/bin/activate
+        # pip install django
+        # pip install mysqlclient
+
+        # django-admin startproject playlist_manager .
+        # django-admin startapp playlists
+
+        # mkdir database && cd database
+        # touch 01_create_database.sql
+
+        # docker build -t playlist-manager-db .
+
+        # docker run -d -p 3306:3306 --name=playlist-manager-mysql-container -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=playlist_manager_database playlist-manager-db
+        pass
+
+    pass
