238e71b2cdf2965086897661fe532fe751607364
calebeLAR <ca6ele@gmail.com>
2023-10-08 15:17:25 -0300

    trybe.python.seção05.dia03.pagina05

---
+++ secao05_django_1/dia03-formularios-e-relacionamento-de-modelos/pagina05/pagina05c.py
@@ -0,0 +1,17 @@
+class Pagina05:
+    def Renderizando_formulários_em_templates():
+        pass
+
+    def Novo_registro_a_partir_de_um_formulário():
+        pass
+
+    def Formulários_e_templates():
+        pass
+
+    def Para_fixar():
+        pass
+
+    def Exercício03():
+        pass
+
+    pass
