0c34eca9ee8b84fb1d1842b32bfc1940ef6d9477
calebeLAR <ca6ele@gmail.com>
2023-10-10 08:58:41 -0300

    trybe.python.seção05.dia04.pagina02

---
+++ secao05_django_1/dia04-django-rest-framework-aula/pagina02/pagina02c.py
@@ -0,0 +1,11 @@
+class Pagina02:
+    def O_que_é_o_Django_REST_Framework():
+        # documentação oficial do Django REST Framework
+        # https://www.django-rest-framework.org/
+
+        pass
+
+    def Um_framework_dentro_de_outro_framework():
+        pass
+
+    pass
