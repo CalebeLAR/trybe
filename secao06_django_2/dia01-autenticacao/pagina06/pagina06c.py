aeb94c6bc3c904957822d0a2e542e2493c870ee7
calebeLAR <ca6ele@gmail.com>
2023-10-15 16:59:43 -0300

    trybe.python.seção06.dia01.pagina06

---
+++ secao06_django_2/dia01-autenticacao/pagina06/pagina06c.py
@@ -0,0 +1,11 @@
+class Pagina06:
+    def Token_Authentication():
+        pass
+
+    def Permitindo_a_quem_usa_obter_Tokens():
+        pass
+
+    def Autenticando_com_Tokens():
+        pass
+
+    pass
