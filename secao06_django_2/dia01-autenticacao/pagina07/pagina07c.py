cd20241a87cd6bab07115c4d52947e9af364066d
calebeLAR <ca6ele@gmail.com>
2023-10-15 16:59:48 -0300

    trybe.python.seção06.dia01.pagina07

---
+++ secao06_django_2/dia01-autenticacao/pagina07/pagina07c.py
@@ -0,0 +1,11 @@
+class Pagina07:
+    def SimpleJWT():
+        pass
+
+    def Instalar_rotear_configurar_e_pronto():
+        pass
+
+    def Qual_autenticação_devo_usar():
+        pass
+
+    pass
