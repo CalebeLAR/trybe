02aebc0132952627e0d15ae2d3af7025cffd675b
calebeLAR <ca6ele@gmail.com>
2023-10-15 16:59:37 -0300

    trybe.python.seção06.dia01.pagina05

---
+++ secao06_django_2/dia01-autenticacao/pagina05/marryme/budget/views.py
@@ -0,0 +1,34 @@
+from rest_framework import viewsets
+from .models import Vendor, Marriage, Budget
+from rest_framework.authentication import BasicAuthentication
+from .serializers import VendorSerializer, MarriageSerializer, BudgetSerializer
+from .permissions import IsOwnerOrAdmin
+
+
+class VendorViewSet(viewsets.ModelViewSet):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+    authentication_classes = [BasicAuthentication]
+
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    queryset = Budget.objects.all()
+    serializer_class = BudgetSerializer
+
+
+class MarriageViewSet(viewsets.ModelViewSet):
+    queryset = Marriage.objects.all()
+    serializer_class = MarriageSerializer
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        """
+        Quem for admin vê todos os casamentos.
+        Caso contrário, a pessoa só vê os próprios casamentos.
+        """
+        if self.request.user.is_superuser:
+            return Marriage.objects.all()
+        else:
+            return Marriage.objects.filter(user=self.request.user)
