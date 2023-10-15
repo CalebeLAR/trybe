82007d25d0968a52bb73e78afcf3dd5008547e31
calebeLAR <ca6ele@gmail.com>
2023-10-15 10:20:11 -0300

    trybe.python.seção06.dia01.pagina10.exercicio02

--- secao06_django_2/dia01-autenticacao/pagina10/personals/personals_rest/admin.py
+++ secao06_django_2/dia01-autenticacao/pagina10/personals/personals_rest/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Client, WorkoutPlan
+
+
+class WorkoutPlanInline(admin.StackedInline):
+    model = WorkoutPlan
+
+
+class ClientAdmin(admin.ModelAdmin):
+    inlines = [WorkoutPlanInline]
+
 
 admin.site.site_header = "presonals api_restfull"
+admin.site.register(Client, ClientAdmin)
