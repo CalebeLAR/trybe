2ff4c50f8f7845815f0c0ed20c447085f8c50c14
calebeLAR <ca6ele@gmail.com>
2023-10-16 19:22:17 -0300

    trybe.python.seção06.dia02.pagina02c.py

---
+++ secao06_django_2/dia02-testando-aplicacões-django/pagina02/pagina02c.py
@@ -0,0 +1,14 @@
+class Pagina02:
+    def Revisão_entendendo_um_gerenciador_de_cinemas_em_Django():
+        pass
+
+    def Subindo_o_container_com_o_banco_de_dados():
+        pass
+
+    def Executando_a_aplicação():
+        pass
+
+    def Visão_geral_da_aplicação():
+        pass
+
+    pass
