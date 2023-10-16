db0cdbb1ae6187cafab59e9a104962ce6929cdc8
calebeLAR <ca6ele@gmail.com>
2023-10-16 19:22:17 -0300

    trybe.python.seção06.dia02.pagina07c.py

---
+++ secao06_django_2/dia02-testando-aplicacões-django/pagina07/pagina07c.py
@@ -0,0 +1,11 @@
+class Pagina07:
+    def Testando_a_autenticação():
+        pass
+
+    def Testando_o_retorno_do_token():
+        pass
+
+    def Testando_a_inserção_de_dados_no_banco():
+        pass
+
+    pass
