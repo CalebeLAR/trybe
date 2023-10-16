c25067558a8af109124aad610bd6f24e1e79d573
calebeLAR <ca6ele@gmail.com>
2023-10-16 19:22:17 -0300

    trybe.python.seção06.dia02.pagina04c.py

---
+++ secao06_django_2/dia02-testando-aplicacões-django/pagina04/pagina04c.py
@@ -0,0 +1,14 @@
+class Pagina04:
+    def Fixtures_para_os_testes():
+        pass
+
+    def Implementando_o_conftest_py():
+        pass
+
+    def APIClient():
+        pass
+
+    def Banco_de_dados():
+        pass
+
+    pass
