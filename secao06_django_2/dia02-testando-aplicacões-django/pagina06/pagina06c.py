cfa5f8cc86378127d4c1986033bfe08f10ca46bd
calebeLAR <ca6ele@gmail.com>
2023-10-16 19:22:17 -0300

    trybe.python.seção06.dia02.pagina06c.py

---
+++ secao06_django_2/dia02-testando-aplicacões-django/pagina06/pagina06c.py
@@ -0,0 +1,14 @@
+class Pagina06:
+    def Testando_templates():
+        pass
+
+    def Testando_o_status_code_da_resposta():
+        pass
+
+    def Testando_o_template_renderizado():
+        pass
+
+    def Testando_o_conteúdo_do_template():
+        pass
+
+    pass
