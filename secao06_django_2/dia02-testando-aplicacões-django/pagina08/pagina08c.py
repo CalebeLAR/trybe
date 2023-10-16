ac15ed438910730df58fe5fb2dd68d34c72f25cd
calebeLAR <ca6ele@gmail.com>
2023-10-16 19:22:17 -0300

    trybe.python.seção06.dia02.pagina08c.py

---
+++ secao06_django_2/dia02-testando-aplicacões-django/pagina08/pagina08c.py
@@ -0,0 +1,17 @@
+class Pagina08:
+    def Testando_um_CRUD():
+        pass
+
+    def Testando_a_operação_Read():
+        pass
+
+    def Testando_a_operação_Create():
+        pass
+
+    def Testando_a_operação_Update():
+        pass
+
+    def Testando_a_operação_Delete():
+        pass
+
+    pass
