ee5e133368a8ed3d5fae106080f3f8a33c9b6c83
calebeLAR <ca6ele@gmail.com>
2023-10-21 09:50:30 -0300

    trybe.python.seção06.dia03.pagina04c.py

---
+++ secao06_django_2/dia03-deployment-no-railway-aula-ao-vivo/pagina04/pagina04c.py
@@ -0,0 +1,17 @@
+class Pagina05:
+    def Configuração_do_MySQL_no_Railway():
+        pass
+
+    def Porque_usar_o_MySQL_do_Railway():
+        # AWS RDS
+        # Google Cloud SQL
+        # outras opções gratuitas.
+        pass
+
+    def Criando_o_banco_de_dados_MySQL_no_Railway():
+        pass
+
+    def Preparando_a_aplicação_para_múltiplos_bancos_de_dados():
+        pass
+
+    pass
